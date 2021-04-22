8021ee987d96dfe2374196d2d51acc51210d257a
wonjun0120 <wonjundero@gmail.com>
2021-04-22 21:20:42 +0900

    수정: CICD테스트용 hello 리턴값 수정

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def hello():
-	return 'hello!'
+	return 'hello! cicd is working!!!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=True)
