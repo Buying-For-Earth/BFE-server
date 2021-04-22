d7092711ecf581b783c3a6058422141c48f00ea9
wonjun0120 <wonjundero@gmail.com>
2021-04-22 23:27:29 +0900

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,11 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def hello():
-<<<<<<< HEAD
 	return 'hello! cicd is working!!!'
-=======
-	return 'hello!'
->>>>>>> 0520fe3c0e4a7d0deb85371828c0b7ea963ca0ad
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=True)
