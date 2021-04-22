e301fd1d111d8483eebace35b4f7dcda3d23432c
wonjun0120 <wonjundero@gmail.com>
2021-04-22 23:44:28 +0900

    수정: 배포 테스트 완료 (#2)

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def hello():
-	return 'hello! cicd is working!!! WA! '
+	return 'hello! cicd is working!!!\n welcome to buying for earth server! '
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=True)
