9cd4ca4ea103a5c457541fb3de4d243b9f58ca04
wonjun0120 <wonjundero@gmail.com>
2021-04-27 19:37:38 +0900

    수정(#8) rds주소 삭제

--- config.py
+++ config.py
@@ -2,12 +2,11 @@ class Config(object):
     DEBUG = False
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    SQLALCHEMY_POOL_RECYCLE=20
 
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = ''
-
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = ''
