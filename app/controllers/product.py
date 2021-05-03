1f52a0b95f451edee1fa957040bb4b115223320e
wonjun0120 <wonjundero@gmail.com>
2021-05-03 23:02:57 +0900

    생성(#25)product api제작

--- app/controllers/product.py
+++ app/controllers/product.py
@@ -1,16 +1,18 @@
 from flask_restx import Resource, Namespace, fields
 from app.utils.product import *
 
+from app.services.product_service import get_product_by_id
+
 api = ProductDto.product
 product_thumbnail_output_field = ProductDto.product_thumbnail_output_field
 product_detail_output_field = ProductDto.product_detail_output_field
 
-@api.route('')
+@api.route('/<int:prod_id>')
 class Product(Resource):
     @api.response(200, 'Success', product_detail_output_field)
-    def get(self):
+    def get(self, prod_id):
         """ 제품 상세 정보를 보여주는 api 입니다 """
+        
+        prod = get_product_by_id(prod_id)
 
-        return{
-            "":""
-        },200
+        return prod,200
--- app/utils/product.py
+++ app/utils/product.py
@@ -19,18 +19,17 @@ class ProductDto:
     })
 
     product_option_content = product.model('Product_option',{
-        'input_type': fields.String(description=''),
         'order_num':fields.Integer(description='해당 옵션의 정렬 순서입니다'),
         'input_option':fields.String(description='해당 옵션에 대한 json 정보입니다')
     })
 
     product_detail_output_field = product.model('Product Detail',{
-        'image': fields.String(description='변경될 예정입니다. 사진을 어떻게 전송할지 리서치가 필요합니다, 썸네일입니다'),
+        'thumbnail': fields.String(description='썸네일의 url입니다'),
         'name': fields.String(description='제품의 이름입니다'),
         'category': fields.String(description='제품의 카테고리입니다'),
         'price': fields.Integer(description='제품의 가격입니다'),
         'detail': fields.List(fields.Nested(product_detail_content), description='본문 구성입니다'),
-        'option': fields.List(fields.Nested(product_option_content), description='제품의 옵션 입니다')
+        'options': fields.List(fields.Nested(product_option_content), description='제품의 옵션 입니다')
     })
 
     
