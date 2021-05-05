68e8f19240dbb72daf7c1c66ea6fc4588da7169c
wonjun0120 <wonjundero@gmail.com>
2021-05-05 15:59:40 +0900

    수정(#35)port 8000 -> 5000

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from app import app
 import os
 
-port = int(os.environ.get("PORT", 8000))
+port = int(os.environ.get("PORT", 5000))
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0', port=port)
